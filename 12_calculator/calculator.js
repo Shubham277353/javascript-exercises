812bd0df40aa6f251a991c1327c29f0425e623b7
Shubham277353 <golubhandari405@gmail.com>
2025-07-25 19:13:14 +0530

    solve 12_calculator exercise

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  let sum = 0;
+  array.forEach((num) => {
+    sum += num;
+  });
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  let mul = 1;
+  arr.forEach((num) => {
+    mul *= num;
+  });
+  return mul;
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  let pow = 1;
+  for (let i = 0; i < b; i++) {
+    pow *= a;
+  }
+  return pow;
 };
 
-const factorial = function() {
-	
+const factorial = function (a) {
+  let factorial = 1 ;
+  for (let i = a ; i > 0; i--) {
+    factorial *= i;
+  }
+  return factorial;
 };
 
 // Do not edit below this line
@@ -29,5 +45,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
